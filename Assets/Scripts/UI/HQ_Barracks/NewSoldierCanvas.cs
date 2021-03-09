1297c560af483d54d1a0ec865cb98c3a6d78151e
LeBanc <ben.ledirach@gmail.com>
2021-03-09 20:11:43 +0100

    Bug Fix: Soldiers have now the lvl1 XP amount when the 'Basic Training' facility is unlocked

--- Assets/Scripts/UI/HQ_Barracks/NewSoldierCanvas.cs
+++ Assets/Scripts/UI/HQ_Barracks/NewSoldierCanvas.cs
@@ -10,6 +10,9 @@ public class NewSoldierCanvas : UICanvas
     public Image soldierAvatar;
     public Button validateButton;
 
+    /// <summary>
+    /// At Awake, init Canvas and subscribe to event
+    /// </summary>
     protected override void Awake()
     {
         base.Awake();
@@ -17,11 +20,17 @@ public class NewSoldierCanvas : UICanvas
         PlayManager.OnRecruit += Show;
     }
 
+    /// <summary>
+    /// OnDestroy, unsubscribe events
+    /// </summary>
     private void OnDestroy()
     {
         PlayManager.OnRecruit -= Show;
     }
 
+    /// <summary>
+    /// Show method generates a new soldier name and avatar and display the Canvas
+    /// </summary>
     public override void Show()
     {
         soldierNameText.text = PlayManager.GetRandomSoldierName();
@@ -32,14 +41,24 @@ public class NewSoldierCanvas : UICanvas
         validateButton.Select();
     }
 
+    /// <summary>
+    /// Recruit method add the new soldier to the PlayManager list and hides the Canvas
+    /// </summary>
     public void Recruit()
     {
+        // Create new soldier with chosen name and avatar
         Soldier _soldier = ScriptableObject.CreateInstance("Soldier") as Soldier;
         _soldier.InitData(Resources.Load("SoldierData/0_Basic_SoldierData") as SoldierData);
         _soldier.ChangeName(soldierNameText.text);
         _soldier.ChangeImage(soldierAvatar.sprite);
+
+        // Add XP to soldier if the facility is unlocked
+        if (PlayManager.recruitingWithXP > 0) _soldier.CurrentXP += _soldier.MaxXP;
+
+        // Add soldier to the PlayManager list
         PlayManager.soldierList.Add(_soldier);
-        PlayManager.soldierIDList.Add(_soldier.ID);
+        PlayManager.soldierIDList.Add(_soldier.ID);        
+
         Hide();
     }
 }
