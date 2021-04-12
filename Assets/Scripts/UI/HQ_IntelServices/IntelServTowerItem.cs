e61b674600803f5e90b68d928eb938f3c93cab63
LeBanc <ben.ledirach@gmail.com>
2021-04-12 18:02:14 +0200

    New feature: Intelligence Services tab displays the active towers detected by the player

---
+++ Assets/Scripts/UI/HQ_IntelServices/IntelServTowerItem.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// IntelServTowerItem class is for a Tower item of the Intelligence Services Canvas
+/// </summary>
+public class IntelServTowerItem : MonoBehaviour
+{
+    // public UI elements
+    public Text towerLevelText;
+    public Text towerTypeText;
+    public Text positionFromHQText;
+    public Text shortAtkText;
+    public Text middleAtkText;
+    public Text longAtkText;
+    public Text exploAtkText;
+    public Text shortDefText;
+    public Text middleDefText;
+    public Text longDefText;
+    public Text exploDefText;
+
+    /// <summary>
+    /// Setup method sets all the text field of the IntelServTowerItem
+    /// </summary>
+    /// <param name="_tower">Tower from which get the data (Tower)</param>
+    public void Setup(Tower _tower)
+    {
+        TowerData _data = _tower.data;
+
+        towerLevelText.text = _data.towerLevel.ToString();
+        towerTypeText.text = _data.towerType.ToString();
+
+        Vector3 _delta = _tower.transform.position - PlayManager.hq.transform.position;
+        string _posText = ((_delta.z >= 0f) ? "North: " : "South: ") + Mathf.Abs(_delta.z) + " / " + ((_delta.x >= 0f) ? "East: " : "West: ") + Mathf.Abs(_delta.x);
+        positionFromHQText.text = _posText;
+
+        shortAtkText.text = _data.shortRangeAttack.ToString();
+        middleAtkText.text = _data.middleRangeAttack.ToString();
+        longAtkText.text = _data.longRangeAttack.ToString();
+        exploAtkText.text = _data.explosiveAttack.ToString();
+
+        shortDefText.text = _data.shortRangeDefense.ToString();
+        middleDefText.text = _data.middleRangeDefense.ToString();
+        longDefText.text = _data.longRangeDefense.ToString();
+        exploDefText.text = _data.explosiveDefense.ToString();
+    }
+}
---
+++ Assets/Scripts/UI/HQ_IntelServices/IntelServiceCanvas.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// IntelServiceCanvas class is the Canvas of the Intelligence Services HQ tab
+/// </summary>
+public class IntelServiceCanvas : UICanvas
+{
+    // public UI elements
+    public RectTransform content;
+    public GameObject intelServItemPrefab;
+
+    /// <summary>
+    /// At Start, subscribe to events
+    /// </summary>
+    private void Start()
+    {
+        PlayManager.OnHQPhase += Setup;
+    }
+
+    /// <summary>
+    /// OnDestroy, unsubscribe from events
+    /// </summary>
+    private void OnDestroy()
+    {
+        PlayManager.OnHQPhase -= Setup;
+    }
+
+    /// <summary>
+    /// Setup method is used to clear the previous items and creates new ones for each active tower on the city field
+    /// </summary>
+    public void Setup()
+    {
+        // Remove the previous items
+        foreach (Transform _child in content.transform)
+        {
+            Destroy(_child.gameObject);
+        }
+
+        // Create and setup new tower items
+        foreach (Tower _t in PlayManager.towerList)
+        {
+            if(_t.IsActive() && !_t.IsDestroyed())
+            {
+                GameObject _go = Instantiate(intelServItemPrefab, content);
+                _go.GetComponent<IntelServTowerItem>().Setup(_t);
+            }
+        }
+    }
+}
