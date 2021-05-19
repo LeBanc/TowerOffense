cf2af464206e50d7d584406e8760e1c55dac7097
LeBanc <ben.ledirach@gmail.com>
2021-05-19 16:42:54 +0200

    Bug Fix: InputField not Activated anymore to enable UI navigation with Controller

---
+++ Assets/Scripts/UI/DeactivatedInputField.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DeactivatedInputField : InputField
+{
+    /// <summary>
+    /// OnSelect method only set the InputField as Selected (no Activation for Joystick Controllers handling)
+    /// </summary>
+    /// <param name="eventData"></param>
+    public override void OnSelect(BaseEventData eventData)
+    {
+        DoStateTransition(SelectionState.Selected, true);
+    }
+}
